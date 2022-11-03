c9b795745ce110d7a04d86f36ba3a0ae9cc32203
JgrabowskiUMW <jgrabowk@mail.umw.edu>
2022-11-03 14:35:18 -0400

    GUI background change to white and image added

--- BooksManangmentSystem.java
+++ BooksManangmentSystem.java
@@ -1,9 +1,7 @@
-import javax.swing.*;
-
 public class BooksManangmentSystem {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Button Example!");
-
+        LibraryGUI window = new LibraryGUI();
+        window.createWindow();
     }
 }
--- LibraryGUI.java
+++ LibraryGUI.java
@@ -1,7 +1,30 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class LibraryGUI {
 
     //create window
     public void createWindow() {
+        JFrame frame = new JFrame("LibraryGUI");
+        JPanel panel = new JPanel();
 
+        panel.setBorder(BorderFactory.createEmptyBorder(100,300,300,300));
+        panel.setLayout(new GridLayout(0,1));
+
+        panel.setOpaque(true);
+        panel.setBackground(Color.WHITE);
+
+        ImageIcon image = new ImageIcon(getClass().getResource("bookpic.jpg"));
+        JLabel label = new JLabel(image);
+
+        panel.add(label);
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
